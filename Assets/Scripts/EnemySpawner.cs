a4ca86a56c7f71e4363a950abfc7a670463d0b90
LuniakaAndrew <andrey.luniacka@yandex.ua>
2019-10-23 22:37:24 +0300

    Add EnemySpawner.cs script

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Enemy[] _enemies;
+
+    public List<Enemy> GetEnemies()
+    {
+        return new List<Enemy>(_enemies);
+    }
+}
