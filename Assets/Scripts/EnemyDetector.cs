ef280d452672bafc4ea127481e044c5cb008dd0c
LuniakaAndrew <andrey.luniacka@yandex.ua>
2019-10-23 22:37:07 +0300

    Add EnemyDetector.cs script

---
+++ Assets/Scripts/EnemyDetector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetector : MonoBehaviour
+{
+    public Action<Vector3> onEnemyDetect;
+    [SerializeField] private Player _player;
+    [SerializeField] private float _distanceToEnemy;
+    [SerializeField] private EnemySpawner _enemySpawner;
+    private List<Enemy> _enemies = new List<Enemy>();
+
+    private void Start()
+    {
+        _enemies = _enemySpawner.GetEnemies();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        for (int i = 0; i < _enemies.Count; i++)
+        {
+            if (!_enemies[i].isDetected &&
+                Vector3.Distance(_player.transform.position, _enemies[i].transform.position) <= _distanceToEnemy)
+            {
+                _enemies[i].isDetected = true;
+                onEnemyDetect?.Invoke(_enemies[i].transform.position);
+            }
+        }
+    }
+}
