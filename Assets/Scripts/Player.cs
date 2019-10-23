950a685f97bcea1dee791d3ce11bcf4f3fa922bf
LuniakaAndrew <andrey.luniacka@yandex.ua>
2019-10-23 22:36:13 +0300

    Add Player.cs script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private ShellLauncher _shellLauncher;
+
+    [SerializeField] private EnemyDetector _enemyDetector;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _enemyDetector.onEnemyDetect += FireInEnemy;
+    }
+
+    private void FireInEnemy(Vector3 target)
+    {
+        _shellLauncher.Launch(target);
+    }
+}
