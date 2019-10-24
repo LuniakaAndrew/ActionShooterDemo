921238a90212c7fc77890ffb4608993c8bd80d8b
LuniakaAndrew <andrey.luniacka@yandex.ua>
2019-10-24 21:02:45 +0300

    Make Singleton

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,13 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utils;
 
-public class EnemySpawner : MonoBehaviour
+public class EnemySpawner : SingletonComponent<EnemySpawner>
 {
+    #region Singleton
+
+    public static EnemySpawner Instance
+    {
+        get { return ((EnemySpawner) _Instance); }
+        set { _Instance = value; }
+    }
+
+    #endregion
+
+    #region Parameters
+
     [SerializeField] private Enemy[] _enemies;
+
+    #endregion
+
+    #region Controls
 
     public List<Enemy> GetEnemies()
     {
         return new List<Enemy>(_enemies);
     }
+
+    #endregion
 }
